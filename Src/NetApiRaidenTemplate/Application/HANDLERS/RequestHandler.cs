e3468d5deb881ed38201c4773bbbf7d3c8279d7d
dgzornoza <dgzornoza@dgzornoza.com>
2024-09-02 12:39:05 +0200

    Añadodp reqiest handler

---
+++ Src/NetApiRaidenTemplate/Application/HANDLERS/RequestHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace $safeprojectname$.Handlers;
+
+public abstract class RequestHandler
+{
+    protected RequestHandler(ILogger logger, IMediator mediator, IHttpContextAccessor httpContextAccessor)
+    {
+        Logger = logger;
+        Mediator = mediator;
+        ContextAccessor = httpContextAccessor;
+    }
+
+    protected ILogger Logger { get; }
+    protected IHttpContextAccessor ContextAccessor { get; }
+    protected IMediator Mediator { get; }
+}
---
+++ TemplateApp/NetApiRaiden1/NetApiRaiden1.Application/Infrastructure/Handlers/RequestHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace NetApiRaiden1.Application.Infrastructure.Handlers;
+public abstract class RequestHandler
+{
+    protected RequestHandler(ILogger logger, IMediator mediator, IHttpContextAccessor httpContextAccessor)
+    {
+        Logger = logger;
+        Mediator = mediator;
+        ContextAccessor = httpContextAccessor;
+    }
+
+    protected ILogger Logger { get; }
+    protected IHttpContextAccessor ContextAccessor { get; }
+    protected IMediator Mediator { get; }
+}
